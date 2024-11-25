7cb03e026d9f8d3af539eea198cf6570a4805999
baroel <73051141+akbarulazis@users.noreply.github.com>
2024-11-25 15:26:53 +0700

    minor typo

--- app.py
+++ app.py
@@ -10,35 +10,33 @@ import os
 # load env variables
 # os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
 load_dotenv()
-st.set_page_config(page_title="Bascc Chat APP", page_icon=":robot:")
+st.set_page_config(page_title="Basic Chat APP", page_icon=":robot:")
+st.title("Basic Chat App clone")
 client = OpenAI(api_key =os.getenv('OPENAI_API_KEY') )
 if 'openai_model' not in st.session_state:
     st.session_state['openai_model'] = "gpt-4o"
 
 if 'message' not in st.session_state:
-    st.session_state.message = []
+    st.session_state.messages = []
 
-for message in st.session_state.message:
+for message in st.session_state.messages:
     with st.chat_message(message['role']):
         st.markdown(message['content'])
 
 prompt = st.chat_input('What is up??')
 if prompt:
-    st.session_state.message.append({'role':'user', 'content':prompt})
+    st.session_state.messages.append({'role':'user', 'content':prompt})
     with st.chat_message('user'):
         st.markdown(prompt)
 
-    with st.chat_message('asisstant'):
-        stream =client.chat.completions.create(
-                model = st.session_state['openai_model'],
-                message=[
-                    {'role': m['role'], 'content':m['content']}
-                    for m in st.session_state.message
-
-                    ], stream = True,
-
-
-
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
         )
         response = st.write_stream(stream)
-    st.session_state.message.append({'role':'asisstant','content':response})
+    st.session_state.messages.append({'role':'asisstant','content':response})
